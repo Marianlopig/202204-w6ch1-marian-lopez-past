e5ff75f3f494d4aa61e7ec167efcc99af00591eb
Marian <marianlopig@gmail.com>
2022-05-09 22:21:21 +0200

    connect store with components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import { useSelector } from "react-redux";
 import TaskForm from "./components/TaskForm/TaskForm";
 import TaskList from "./components/TasksList/TaskList";
 
 function App() {
+  const tasks = useSelector((state) => state.tasks);
+  console.log(tasks);
+
   return (
     <>
       <TaskForm />
-      <TaskList />
+      <TaskList tasks={tasks} />
     </>
   );
 }
