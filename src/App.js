bad9a71f1f0cfbd43e06a71f7170909739caf601
Marian <marianlopig@gmail.com>
2022-05-09 21:24:49 +0200

    tasklist component created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import TaskForm from "./components/TaskForm/TaskForm";
+import TaskList from "./components/TasksList/TaskList";
 
 function App() {
-  return <TaskForm />;
+  return (
+    <>
+      <TaskForm />
+      <TaskList />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/TasksList/TaskList.js
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+const TaskList = () => {
+  const [tasks, setTasks] = useState([]);
+
+  return (
+    <div>
+      <ul></ul>
+    </div>
+  );
+};
+export default TaskList;
